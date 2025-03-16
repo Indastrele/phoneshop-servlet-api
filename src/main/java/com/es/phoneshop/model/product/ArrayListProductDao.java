f92dd0b599135dc92802fc15f1b9c33cdf19dda4
Stanislav Yudin <slavachanskoyesalo@gmail.com>
2025-03-16 20:12:11 +0300

    Task 2.3 Insert demodata

--- src/main/java/com/es/phoneshop/model/product/ArrayListProductDao.java
+++ src/main/java/com/es/phoneshop/model/product/ArrayListProductDao.java
@@ -1,43 +1,30 @@
 package com.es.phoneshop.model.product;
 
-import java.math.BigDecimal;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Set;
 import java.util.concurrent.locks.ReentrantReadWriteLock;
 import java.util.stream.Collectors;
 
 public class ArrayListProductDao implements ProductDao {
+    private static ProductDao instance;
+
+    public static synchronized ProductDao getInstance() {
+        if (instance == null) {
+            instance = new ArrayListProductDao();
+        }
+
+        return instance;
+    }
+
     private ArrayList<Product> products = new ArrayList<>();
     private long identity = 1;
 
     private ReentrantReadWriteLock readWriteLock = new ReentrantReadWriteLock();
 
-    public ArrayListProductDao() {
-        initProductList();
-    }
+    private ArrayListProductDao() {
 
-    private void initProductList() {
-        final Currency usd = Currency.getInstance("USD");
-
-        readWriteLock.writeLock().lock();
-        try {
-
-            save(new Product("sgs", "Samsung Galaxy S", new BigDecimal(100), usd, 100, "https://raw.githubusercontent.com/andrewosipenko/phoneshop-ext-images/master/manufacturer/Samsung/Samsung%20Galaxy%20S.jpg"));
-            save(new Product("sgs2", "Samsung Galaxy S II", new BigDecimal(200), usd, 0, "https://raw.githubusercontent.com/andrewosipenko/phoneshop-ext-images/master/manufacturer/Samsung/Samsung%20Galaxy%20S%20II.jpg"));
-            save(new Product("sgs3", "Samsung Galaxy S III", new BigDecimal(300), usd, 5, "https://raw.githubusercontent.com/andrewosipenko/phoneshop-ext-images/master/manufacturer/Samsung/Samsung%20Galaxy%20S%20III.jpg"));
-            save(new Product("iphone", "Apple iPhone", new BigDecimal(200), usd, 10, "https://raw.githubusercontent.com/andrewosipenko/phoneshop-ext-images/master/manufacturer/Apple/Apple%20iPhone.jpg"));
-            save(new Product("iphone6", "Apple iPhone 6", new BigDecimal(1000), usd, 30, "https://raw.githubusercontent.com/andrewosipenko/phoneshop-ext-images/master/manufacturer/Apple/Apple%20iPhone%206.jpg"));
-            save(new Product("htces4g", "HTC EVO Shift 4G", new BigDecimal(320), usd, 3, "https://raw.githubusercontent.com/andrewosipenko/phoneshop-ext-images/master/manufacturer/HTC/HTC%20EVO%20Shift%204G.jpg"));
-            save(new Product("sec901", "Sony Ericsson C901", new BigDecimal(420), usd, 30, "https://raw.githubusercontent.com/andrewosipenko/phoneshop-ext-images/master/manufacturer/Sony/Sony%20Ericsson%20C901.jpg"));
-            save(new Product("xperiaxz", "Sony Xperia XZ", new BigDecimal(120), usd, 100, "https://raw.githubusercontent.com/andrewosipenko/phoneshop-ext-images/master/manufacturer/Sony/Sony%20Xperia%20XZ.jpg"));
-            save(new Product("nokia3310", "Nokia 3310", new BigDecimal(70), usd, 100, "https://raw.githubusercontent.com/andrewosipenko/phoneshop-ext-images/master/manufacturer/Nokia/Nokia%203310.jpg"));
-            save(new Product("palmp", "Palm Pixi", new BigDecimal(170), usd, 30, "https://raw.githubusercontent.com/andrewosipenko/phoneshop-ext-images/master/manufacturer/Palm/Palm%20Pixi.jpg"));
-            save(new Product("simc56", "Siemens C56", new BigDecimal(70), usd, 20, "https://raw.githubusercontent.com/andrewosipenko/phoneshop-ext-images/master/manufacturer/Siemens/Siemens%20C56.jpg"));
-            save(new Product("simc61", "Siemens C61", new BigDecimal(80), usd, 30, "https://raw.githubusercontent.com/andrewosipenko/phoneshop-ext-images/master/manufacturer/Siemens/Siemens%20C61.jpg"));
-            save(new Product("simsxg75", "Siemens SXG75", new BigDecimal(150), usd, 40, "https://raw.githubusercontent.com/andrewosipenko/phoneshop-ext-images/master/manufacturer/Siemens/Siemens%20SXG75.jpg"));
-        }
-        finally {
-            readWriteLock.writeLock().unlock();
-        }
     }
 
     @Override
@@ -58,7 +45,28 @@ public class ArrayListProductDao implements ProductDao {
     public List<Product> findProducts(String query, SortField field, SortOrder order) {
         readWriteLock.readLock().lock();
         try {
-            Comparator<Product> comparator = Comparator.comparing(product -> {
+            Comparator<Product> comparator;
+
+            if (field == null && order == null) {
+                comparator = Comparator.comparingInt(product -> {
+                    if (query == null) {
+                        return 0;
+                    }
+
+                    List<String> tokenizedQuery = List.of(query.split(" "));
+                    List<String> description = List.of(product.getDescription().split(" "));
+                    Set<String> intersections = description.stream()
+                            .distinct()
+                            .filter(tokenizedQuery::contains)
+                            .collect(Collectors.toSet());
+
+                    return tokenizedQuery.size() - intersections.size();
+                });
+
+                return getFilteredProducts(comparator, query);
+            }
+
+            comparator = Comparator.comparing(product -> {
                if (SortField.description == field) {
                    return (Comparable) product.getDescription();
                } else {
@@ -70,44 +78,47 @@ public class ArrayListProductDao implements ProductDao {
                 comparator = comparator.reversed();
             }
 
-            return products.stream()
-                    .filter(product -> {
-                        if (query == null || query.isEmpty()) return true;
-
-                        List<String> tokenizedQuery = List.of(query.split(" "));
-                        List<String> description = List.of(product.getDescription().split(" "));
-                        return tokenizedQuery.stream().allMatch(description::contains);
-                    })
-                    .filter(product -> product.getPrice() != null)
-                    .filter(product -> product.getStock() > 0)
-                    .sorted(Comparator.comparingInt(product -> {
-                        if (query == null) {
-                            return 0;
-                        }
-
-                        List<String> tokenizedQuery = List.of(query.split(" "));
-                        List<String> description = List.of(product.getDescription().split(" "));
-                        Set<String> intersections = description.stream()
-                                .distinct()
-                                .filter(tokenizedQuery::contains)
-                                .collect(Collectors.toSet());
-
-                        return tokenizedQuery.size() - intersections.size();
-                    }))
-                    .sorted(comparator)
-                    .collect(Collectors.toList());
+            return getFilteredProducts(comparator, query);
         }
         finally {
             readWriteLock.readLock().unlock();
         }
     }
 
+    private List<Product> getFilteredProducts(Comparator<Product> comparator, String query) {
+        return products.stream()
+                .filter(product -> {
+                    if (query == null || query.isEmpty()) return true;
+
+                    List<String> tokenizedQuery = List.of(query.split(" "));
+                    List<String> description = List.of(product.getDescription().split(" "));
+                    return tokenizedQuery.stream().allMatch(description::contains);
+                })
+                .filter(product -> product.getPrice() != null)
+                .filter(product -> product.getStock() > 0)
+                .sorted(comparator)
+                .collect(Collectors.toList());
+    }
+
     @Override
     public void save(Product product) {
         readWriteLock.writeLock().lock();
         try {
-            product.setId(identity++);
-            products.add(product);
+            var id = product.getId();
+            if (id == null) {
+                product.setId(identity++);
+                products.add(product);
+                return;
+            }
+
+            var oldProduct = getProduct(id);
+            if (oldProduct == null) {
+                throw new ProductNotFoundException(id);
+            }
+
+            var index = products.indexOf(oldProduct);
+            product.setId(oldProduct.getId());
+            products.set(index, product);
         }
         finally {
             readWriteLock.writeLock().unlock();
